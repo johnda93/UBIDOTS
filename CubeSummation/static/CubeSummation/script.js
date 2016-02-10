25ec763deb01ee255a4a0f570ab5f9a1319c9907
JohnDa <jodcastrori@unal.edu.co>
2016-02-10 18:09:53 -0500

    Adding main containers and button functionality

--- CubeSummation/static/CubeSummation/script.js
+++ CubeSummation/static/CubeSummation/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
+	$('select').material_select();
+});
 
+$('#go_btn').on('click', function () {
+	if ($('#test_cases').hasClass('invalid')) {
+		Materialize.toast('Please select a valid number of test cases', 4000, 'rounded toast_error');
+	} else {
+		$('#go_btn').addClass('disabled');
+		$('#reset_btn').removeClass('disabled');
+		$('main .container .styled').show();
+	}
 });
