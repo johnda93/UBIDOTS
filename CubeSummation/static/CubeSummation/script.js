224cdb2506928efd3f85f7afe26539fb77c7b219
JohnDa <jodcastrori@unal.edu.co>
2016-02-10 18:36:36 -0500

    Adding error validation

--- CubeSummation/static/CubeSummation/script.js
+++ CubeSummation/static/CubeSummation/script.js
@@ -3,8 +3,19 @@ $(document).ready(function () {
 });
 
 $('#go_btn').on('click', function () {
-	if ($('#test_cases').hasClass('invalid')) {
-		Materialize.toast('Please select a valid number of test cases', 4000, 'rounded toast_error');
+	$test_cases = $('#test_cases');
+	$test_cases_label = $test_cases.parent().parent().parent().find('label');
+
+	if ($test_cases.val().length === 0) {
+		$test_cases.addClass('invalid');
+        $test_cases_label.addClass('active');
+        $test_cases_label.attr('data-error', "Required");
+
+        Materialize.toast('Please enter a valid number of test cases', 4000, 'rounded toast_error');
+	} else if ($test_cases.hasClass('invalid')) {
+		$test_cases_label.attr('data-error', "Invalid");
+
+		Materialize.toast('Please enter a valid number of test cases', 4000, 'rounded toast_error');
 	} else {
 		$('#go_btn').addClass('disabled');
 		$('#reset_btn').removeClass('disabled');
