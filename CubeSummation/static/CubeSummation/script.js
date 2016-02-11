1b2c252a1cd408ac7b2936aa4c640c97525a8bb2
JohnDa <jodcastrori@unal.edu.co>
2016-02-10 20:51:14 -0500

    Adding functionality to button Go! (Creation of test cases)

--- CubeSummation/static/CubeSummation/script.js
+++ CubeSummation/static/CubeSummation/script.js
@@ -19,6 +19,39 @@ $('#go_btn').on('click', function () {
 	} else {
 		$('#go_btn').addClass('disabled');
 		$('#reset_btn').removeClass('disabled');
-		$('main .container .styled').show();
+
+		createTestCases($('#test_cases').val());
 	}
 });
+
+function createTestCases (number_test_cases) {
+	$row_test_cases = $('#row_test_cases');
+
+	for (var i = 1; i <= number_test_cases; i++) {
+		$aux_class = '';
+
+		if (i === 1) {
+			$aux_class = ' active';
+		}
+
+		$div_test_case = '<div class="col l5 s12' + $aux_class + '">' +
+	  						'<div id="test_case_' + i + '" class="styled container">' +
+	  							'<h5>Test Case #' + i +
+	  								'<div><a href="#"><i class="material-icons center">chevron_left</i></a>' + '1 / ' + number_test_cases + '<a href="#"><i class="material-icons center">chevron_right</i></a></h5></div>'
+	  						'</div>' +
+	  					  '</div>';
+
+		$row_test_cases.append($div_test_case);
+	}
+
+	$div_test_case = '<div class="col l5 offset-l2 s12">' +
+	  					'<div id="results" class="styled container">' +
+	  						'<h5>Results!</h5>' + 
+	  					'</div>' +
+	  				 '</div>';
+
+	$row_test_cases.append($div_test_case);
+
+	$('#test_case_1').show();
+	$('#results').show();
+}
